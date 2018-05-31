2a06f1459dd0453036ef179e01a49e969dde7102
lukegood <wanglgabc@163.com>
2018-05-31 09:29:38 +0800

    添加好了数据库

--- app/src/main/java/com/example/rubrub/MainActivity.java
+++ app/src/main/java/com/example/rubrub/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.rubrub;
-
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,6 +15,9 @@ import com.example.rubrub.Fragment.NeighbourFragment;
 import com.example.rubrub.Fragment.SearchFragment;
 
 import java.util.ArrayList;
+import cn.bmob.v3.Bmob;
+import android.app.Activity;
+import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +28,7 @@ private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bmob.initialize(this, "14b82d3140bd6401d0584482b4015795");
         setContentView(R.layout.activity_main);
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
