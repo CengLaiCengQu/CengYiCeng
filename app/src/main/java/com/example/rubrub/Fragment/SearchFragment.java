6a6f661a38857cdd4d0633c1a45e3ab4c3ffb80c
wanga <1349874293@qq.com>
2018-05-31 08:30:03 +0800

    改为Fragment，数据库依赖添加了一些

---
+++ app/src/main/java/com/example/rubrub/Fragment/SearchFragment.java
@@ -0,0 +1,28 @@
+package com.example.rubrub.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rubrub.R;
+
+public class SearchFragment extends Fragment {
+    private AppCompatActivity appCompatActivity;
+
+    public AppCompatActivity getAppCompatActivity() {
+        return appCompatActivity;
+    }
+
+    public void setAppCompatActivity(AppCompatActivity appCompatActivity) {
+        this.appCompatActivity = appCompatActivity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view =inflater.inflate(R.layout.search_fragment_xml,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/rubrub/MainActivity.java
+++ app/src/main/java/com/example/rubrub/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.rubrub;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 import android.content.Intent;
-import android.view.*;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
 
+import com.example.rubrub.Fragment.AnnouncementFragment;
+import com.example.rubrub.Fragment.MeFragment;
+import com.example.rubrub.Fragment.NeighbourFragment;
+import com.example.rubrub.Fragment.SearchFragment;
 
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,28 +31,51 @@ private BottomNavigationView bottomNavigationView;
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                Fragment fragment = null;
+                ArrayList<Fragment> list = new ArrayList<>();
+                SearchFragment searchFragment =new SearchFragment();
+                AnnouncementFragment announcementFragment=new AnnouncementFragment();
+                NeighbourFragment neighbourFragment=new NeighbourFragment();
+                MeFragment meFragment =new MeFragment();
+                list.add(searchFragment);
+                list.add(announcementFragment);
+                list.add(neighbourFragment);
+                list.add(meFragment);
                 switch (item.getItemId()) {
                     case R.id.search:
-                        Intent intent =new Intent(MainActivity.this,SearchActivity.class);
-                        startActivity(intent);
+                        fragment = new SearchFragment();
+                        transaction.replace(R.id.search,fragment);
+                        transaction.commit();
+                        //Intent intent =new Intent(MainActivity.this,SearchActivity.class);
+                        //startActivity(intent);
                         break;
                     case R.id.neighbour:
-                        Intent intent1 =new Intent(MainActivity.this,NeighbourActivity.class);
-                        startActivity(intent1);
+                        //fragment = new NeighbourFragment();
+                        //transaction.replace(R.id.neighbour,fragment);
+                        //transaction.commit();
+                        //Intent intent1 =new Intent(MainActivity.this,NeighbourActivity.class);
+                        //startActivity(intent1);
                         break;
                     case R.id.board:
-                        Intent intent2 =new Intent(MainActivity.this,AnnouncementActivity.class);
-                        startActivity(intent2);
+                        //fragment =new AnnouncementFragment();
+                        //transaction.replace(R.id.board,fragment);
+                        //transaction.commit();
+                        //Intent intent2 =new Intent(MainActivity.this,AnnouncementActivity.class);
+                        //startActivity(intent2);
                         break;
                     case R.id.me:
-                        Intent intent3 =new Intent(MainActivity.this,MeActivity.class);
-                        startActivity(intent3);
+                        //fragment = new MeFragment();
+                        //transaction.replace(R.id.me,fragment);
+                        //transaction.commit();
+                        //Intent intent3 =new Intent(MainActivity.this,MeActivity.class);
+                        //startActivity(intent3);
                         break;
                 }
                 return true;
             }
         });
-
 
     }
 
