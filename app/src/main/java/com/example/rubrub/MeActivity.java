dc88cd414dc716b39dcf855ef15b82ee7fad59ba
Aurora <2224208712@qq.com>
2018-06-08 23:32:15 +0800

    加了一个我的界面里的设置，查看按钮，设置界面里面加入了相应部件，能够实现跳转

--- app/src/main/java/com/example/rubrub/MeActivity.java
+++ app/src/main/java/com/example/rubrub/MeActivity.java
@@ -1,7 +1,10 @@
 package com.example.rubrub;
 import android.app.Fragment;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MeActivity  extends FragmentActivity {
 
@@ -9,5 +12,10 @@ public class MeActivity  extends FragmentActivity {
     protected void onCreate(android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.me_xml);
+
+
+
+
+
         }
 }
