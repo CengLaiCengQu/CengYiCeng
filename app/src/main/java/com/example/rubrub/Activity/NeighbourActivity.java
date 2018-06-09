e2a1b79946c98ba55952cf41e910fe65d386416c
wanga <1349874293@qq.com>
2018-06-09 11:50:36 +0800

    添加关于搜索需要的活动和界面以及上传不见了的社区活动

---
+++ app/src/main/java/com/example/rubrub/Activity/NeighbourActivity.java
@@ -0,0 +1,15 @@
+package com.example.rubrub.Activity;
+
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rubrub.R;
+
+public class NeighbourActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.neighbour_xml);
+        }
+}
---
+++ app/src/main/java/com/example/rubrub/Activity/SearchbytimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.rubrub.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rubrub.R;
+
+public class SearchbytimeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchbytime_xml);
+    }
+}
